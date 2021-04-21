2bbabd4ba0b8897b9df49b15acc580bb10496774
aklezovics <arturs.klezovics@raisin.com>
2021-04-21 19:15:38 +0200

    Initial commit

---
+++ src/main/java/com/klezovich/springtestcoaching/SpringTestCoachingApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.springtestcoaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCoachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestCoachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/springtestcoaching/SpringTestCoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.springtestcoaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCoachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
